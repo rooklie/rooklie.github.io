f1d95dfb906452662547a0529ec8b237de1b5cc3
rookshire <45058018+rooklie@users.noreply.github.com>
2020-02-28 15:20:02 -0600

    working on shoting mechanisim

--- interactive scene/sketch.js
+++ interactive scene/sketch.js
@@ -5,26 +5,34 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let objspeedx = 1;
+let objspeedx = 0;
 let objspeedy = 0;
 let objposx = 10;
-let objposy = 10;
+console.log (windowheight);
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
+let objposy = height - 10;
 
 function draw() {
   background(220);
+fill ("red");
+  //shoot();
 objex();
 
 }
 
-function movment(){
+function shoot(){
+  let basex = 0;
+  let basey = windowheight ;
+  tempx = mouseX - basex ;
+  tempy = mousey - basey ;
+  tempy = tempy * -1;
 
-
-
+  objspeedx = tempx /100;
+  objspeedy = tempy /100;
 
 }
 
@@ -38,11 +46,11 @@ function objex() {
 
   ellipse (objposx, objposy, 10, 10)
   
-  objspeedy = objspeedy + 0.005;
-  objposy = objposy + objspeedy;
+  //objspeedy = objspeedy + 0.005;
   
-
-  objposx = objposx + objspeedx;
+  //objposy = objposy + objspeedy;
+  
+  //objposx = objposx + objspeedx;
 
 
 
